c151fe101ad7204ed2a296778ae8d58e060146fc
BUDZ1974 <sasabudz@gmail.com>
2021-10-18 20:04:24 +0300

    dz
    dz

---
+++ Сiphers/Encryption.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Сiphers
+{
+    class Encryption
+    {
+        const string alf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        #region Caeser
+
+      
+        public static string Caeser(string input, int key)
+        {
+            var res = new StringBuilder();
+            input = input.ToUpper();
+          
+            for (int i = 0; i < input.Length; i++)
+                {
+                   for (int j = 0; j < alf.Length; j++)
+                   {
+                    if (input[i] == alf[j])
+                    {
+                        res.Append(alf[(j + key) % alf.Length]);
+                    }
+                   }
+
+                }
+            return res.ToString();
+        }
+        public static string DeCaeser(string input, int key=0)
+        {
+            var res = new StringBuilder();
+            input = input.ToUpper();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < alf.Length; j++)
+                {
+                    if (input[i] == alf[j])
+                    {
+                        res.Append(alf[(j - key + alf.Length) % alf.Length]);
+                    }
+                }
+
+            }
+            return res.ToString();
+        }
+#endregion
+
+        #region Vigenère
+
+
+        public static string Encrypt(string input, string key)
+        {
+            string EnglishAlph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            input = input.ToUpper();
+            key = key.ToUpper();
+            var sb = new StringBuilder();
+            int counter = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!(char.IsLetter(input[i])))
+                {
+                    sb.Append(input[i]);
+                    counter++;
+                }
+                else
+                {
+                    var inputCharIndex = EnglishAlph.IndexOf(input[i]);
+                    var keyIndex = (i - counter) % key.Length;
+                    var decryptedChar = EnglishAlph[(inputCharIndex + EnglishAlph.IndexOf(key[keyIndex]))
+                    % EnglishAlph.Length];
+                    sb.Append(decryptedChar);
+                }
+            }
+            return sb.ToString(); ;
+
+        }
+        public static string Decrypt(string input, string key)
+        {
+            string EnglishAlph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            input = input.ToUpper();
+            key = key.ToUpper();
+            var sb = new StringBuilder();
+            int counter = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!(char.IsLetter(input[i])))
+                {
+                    sb.Append(input[i]);
+                    counter++;
+                }
+                else
+                {
+                    var inputCharIndex = EnglishAlph.IndexOf(input[i]);
+                    var keyIndex = (i - counter) % key.Length;
+                    var decryptedChar = EnglishAlph[(inputCharIndex + EnglishAlph.Length - EnglishAlph.IndexOf(key[keyIndex]))
+                    % EnglishAlph.Length];
+                    sb.Append(decryptedChar);
+                }
+            }
+            return sb.ToString();
+        }
+ #endregion
+    }
+}
---
+++ Сiphers/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Сiphers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        budz:
+            Console.WriteLine("Выберите Шифр" +
+                "              Виженер -- 1" +
+                "              Цезарь  -- 2 ");
+
+            string choice = Convert.ToString(Console.ReadLine());
+            switch (choice)
+            {
+                case  "1" :
+                    Console.WriteLine("Введите строку");
+                    string Encryp = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Введите ключ");
+                    string key = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine($"Шифр Виженера -- {Encryption.Encrypt(Encryp, key)}");
+      
+                    Console.WriteLine($"Decode Виженера -- {Encryption.Decrypt(Encryp, key)}");
+                    Console.ReadLine();
+                    Console.Clear();
+                    goto budz;
+                case "2":
+                    Console.WriteLine("Введите строку");
+                    string CeaserEncryp = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Введите ключ");
+                    int CeaserKey = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine($"Шифр Цезаря -- {Encryption.Caeser(CeaserEncryp, CeaserKey)}");
+                    Console.WriteLine($"decode Цезаря -- {Encryption.DeCaeser(CeaserEncryp, CeaserKey)}");
+                    Console.ReadLine();
+                    Console.Clear();
+                    goto budz;
+                default:
+                    Console.WriteLine("такой команды нет ошибка ");
+                    goto budz;
+            }
+
+            
+        }
+    }
+}
